42160d8c7c92710ad4b8eef47b5a82f488b14667
RBJervey <jerrebe@gmail.com>
2026-08-24 20:47:46 -0400

    Rebecca Input Percent

--- plugins/automation/src/main/kotlin/app/aaps/plugins/automation/elements/InputPercent.kt
+++ plugins/automation/src/main/kotlin/app/aaps/plugins/automation/elements/InputPercent.kt
@@ -24,7 +24,7 @@ class InputPercent() : Element {
 
     companion object {
 
-        const val MIN = 50.0
-        const val MAX = 130.0
+        const val MIN = 00.0
+        const val MAX = 200.0
     }
 }
